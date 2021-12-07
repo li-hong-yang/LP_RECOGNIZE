d61a4622644c246ad9b99bea58d1a2ebae3180d0
lihongyang <lihongyang@hongyang.li>
2021-12-07 04:16:09 -0800

    delete #

--- detcet.sh
+++ detcet.sh
@@ -1,1 +1,1 @@
-# python detect.py --source /home/hyli/project/yolov5-master/2.jpg --weights /home/hyli/project/yolov5-master/runs/train/exp98/weights/best.pt --conf 0.25
+python detect.py --source /home/hyli/project/yolov5-master/2.jpg --weights /home/hyli/project/yolov5-master/runs/train/exp98/weights/best.pt --conf 0.25
